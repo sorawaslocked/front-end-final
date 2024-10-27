ab1358cb8a1e94c994cbc3dc8174987dda0fe703
Нургиса <230780@astanait.edu.kz>
2024-10-28 02:04:19 +0600

    little changes

--- components/cart_items.js
+++ components/cart_items.js
@@ -3,21 +3,18 @@ function generateCartItems() {
     <table class="table-sm table-hover">
       <tr>
         <td class="text-short">
-          <img src="images/drinks/actual_peach_passionfruit.jpg" class="cart-min-img" alt="">
           Актуаль Персик Маракуйя 1л
         </td>
         <td>10шт</td>
       </tr>
       <tr>
         <td class="text-short">
-          <img src="images/lays_chili_lime.jpg" class="cart-min-img" alt="">
           Lays Чили Лайм 200г
         </td>
         <td>5шт</td>
       </tr>
       <tr>
         <td class="text-short">
-          <img src="images/fruits-berries/tashkent_lemon.png" class="cart-min-img" alt="">
           Ташкентские Лимоны
         </td>
         <td>2шт</td>
