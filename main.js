2961b08f67044b774d0c7e1f2d673b36f724b5e5
sora <133684349+sorawaslocked@users.noreply.github.com>
2024-10-30 20:43:58 +0500

    Fix profile script loading issues

--- main.js
+++ main.js
@@ -1,7 +1,3 @@
 const DB_FROM_LOCAL_JSON = localStorage.getItem("db");
 const DB = DB_FROM_LOCAL_JSON ? JSON.parse(DB_FROM_LOCAL_JSON) : fakeDb;
 const USER_LOGGED_IN = Number.parseInt(sessionStorage.getItem('loggedId')) || 0;
-
-
-console.log("USER_LOGGED_IN:", USER_LOGGED_IN); // Проверяем ID пользователя
-const currentUser = USERS_FROM_DB.find(user => user.id === USER_LOGGED_IN);
